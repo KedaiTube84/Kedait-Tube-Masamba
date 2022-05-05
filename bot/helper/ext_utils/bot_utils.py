ee4c2691ea8a0c2ca73be1c7099cdb66e936f4c1
JagoanMasamba <96615123+JagoanMasamba@users.noreply.github.com>
2022-05-05 15:33:21 +0700

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -137,12 +137,10 @@ def get_readable_message():
                 else:
                     msg += f"\n<b>Downloaded:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
                 msg += f"\n<b>Speed:</b> {download.speed()} | <b>ETA:</b> {download.eta()}"
+                msg += f"\n<b>Engine:</b> {download.aria_download().connections}"
                 try:
                     msg += f"\n<b>Seeders:</b> {download.aria_download().num_seeders}" \
-                           f" | <b>Peers:</b> {download.aria_download().connections}"
-               except:
-                    pass
-                    msg += f"\n<b>Engine:</b> {download.aria_download().connections}"
+                           f" | <b>Peers:</b> {download.aria_download().connections}"       
                except:
                     pass    
                     msg += f"\n<b>Seeders:</b> {download.torrent_info().num_seeds}" \
