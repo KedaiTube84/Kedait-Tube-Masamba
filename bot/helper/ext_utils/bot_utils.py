f77e754af1bdd8811d1d6496837dfefd4632203e
JagoanMasamba <96615123+JagoanMasamba@users.noreply.github.com>
2022-05-05 15:12:49 +0700

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -147,7 +147,7 @@ def get_readable_message():
                            f" | <b>Leechers:</b> {download.torrent_info().num_leechs}"
                 except:
                     pass
-                msg += f"\n<b>Engine: qBittorrent v4</b>\n<b>Peers:</b> {download.aria_download().connections}"
+                msg += f"\n<b>Engine:</b> {download.aria_download().connections}"
                 msg += f"\n<b>Adder:</b> {download.message.from_user.first_name} <code>({download.message.from_user.id})</code>"
                 msg += f"\n<b>Stop:</b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"				
             elif download.status() == MirrorStatus.STATUS_SEEDING:
